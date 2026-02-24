11f93dc8cba72403d7c150b96737a77e9b29126d
premices9-cmd <premices9@gmail.com>
2026-02-24 13:51:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 
 class MonApp(App):
     def build(self):
-        return Label(text="Application Mobile Money OK")
+        return Label(text="Application de Gestion")
 
 if __name__ == "__main__":
     MonApp().run()
